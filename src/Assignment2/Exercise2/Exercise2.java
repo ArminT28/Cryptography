17b93d876d9fb5980572a92bb02a8651e499974b
Torok Armin <80058551+ArminT28@users.noreply.github.com>
2024-03-28 12:13:31 +0200

    Updated Assignment2 Exercise2

--- src/Assignment2/Exercise2/Exercise2.java
+++ src/Assignment2/Exercise2/Exercise2.java
@@ -36,10 +36,69 @@ public class Exercise2 {
             char frequencyCharacter2 = secondPair.charAt(0);
             char occurenceCharacter2 = secondPair.charAt(2);
 
-            int keyA = occurenceCharacter1 - frequencyCharacter1;
-            int keyB = occurenceCharacter2 - frequencyCharacter2;
+            int x1 = frequencyCharacter1 - 'A';
+            int y1 = occurenceCharacter1 - 'A';
+            int x2 = frequencyCharacter2 - 'A';
+            int y2 = occurenceCharacter2 - 'A';
 
-            System.out.println("KeyA: " + keyA + " KeyB: " + keyB);
+            List<List<Integer>> solutions = solveMathematicalSystem(x1, y1, x2, y2);
+            if(solutions !=null) {
+                for (List<Integer> singleSolution : solutions) {
+                    int gcd = gcd(singleSolution.get(0), CONFIG_ALPHABET_SIZE);
+                    if (gcd == 1) {
+                        int a = singleSolution.get(0);
+                        int b = singleSolution.get(1);
+                        System.out.println("For the system \t{" + x1 + "a + b = " + y1);
+                        System.out.println("\t\t\t\t{" + x2 + "a + b = " + y2);
+                        System.out.println("Found a solution a: " + a + " and b: " + b);
+                        String plainText = Decryption(a,b,cipherText,CONFIG_ALPHABET_SIZE);
+                        System.out.println(plainText + "\n");
+
+                        ///Check if the plaintext has most common Di-, Tri- and Quadrigrams
+                        ///If at least 4 Digramms and 2 Trigramm || 2 Quadrigramm is found we found the answer
+                        if (numberOfQuadrigrams(plainText)>=2) {
+                            System.out.println("Found at least two Quadrigramms for a:" + a + " b:" + b);
+                            System.out.println(plainText);
+                            System.out.print("Is PlainText readable? (Y/N): ");
+                            Scanner scanner = new Scanner(System.in);
+                            String answer = scanner.nextLine();
+                            if (answer.equals("Y")) {
+                                return plainText;
+                            }
+                        }
+                        else if (numberOfTrigramms(plainText)>=4) {
+                            System.out.println("Found at least four Trigramms for a:" + a + " b:" + b);
+                            System.out.println(plainText);
+                            System.out.print("Is PlainText readable? (Y/N): ");
+                            Scanner scanner = new Scanner(System.in);
+                            String answer = scanner.nextLine();
+                            if (answer.equals("Y")) {
+                                return plainText;
+                            }
+                        }
+                        else if (numberOfTrigramms(plainText)>=2 && numberOfDigramms(plainText)>=4) {
+                            System.out.println("Found at least two Trigramms and at least four Digramms for a:" + a + " b:" + b);
+                            System.out.println(plainText);
+                            System.out.print("Is PlainText readable? (Y/N): ");
+                            Scanner scanner = new Scanner(System.in);
+                            String answer = scanner.nextLine();
+                            if (answer.equals("Y")) {
+                                return plainText;
+                            }
+                        }
+                        else if (numberOfDigramms(plainText)>=6) {
+                            System.out.println("Found at least six Digramms for a:" + a + " b:" + b);
+                            System.out.println(plainText);
+                            System.out.print("Is PlainText readable? (Y/N): ");
+                            Scanner scanner = new Scanner(System.in);
+                            String answer = scanner.nextLine();
+                            if (answer.equals("Y")) {
+                                return plainText;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         return "Failed to decrypt.";
@@ -119,4 +178,124 @@ public class Exercise2 {
         }
         return combinations;
     }
+
+    public static List<List<Integer>> solveMathematicalSystem(double x1, double y1, double x2, double y2) {
+        List<List<Integer>> solutions = new ArrayList<>();
+
+        if (x1 == x2) {
+            if (y1 == y2) {
+                return null; // Infinite solutions
+            } else {
+                return null; // No solution
+            }
+        } else {
+            double a = (y1 - y2) / (x1 - x2);
+            double b = y1 - ( x1 * a );
+
+            // Round the coefficients to integers
+            int roundedA = (int)Math.round(a);
+            int roundedB = (int)Math.round(b);
+
+            // Check if the rounded coefficients are equal to the original ones
+            if (Math.abs(roundedA - a) < 1e-6 && Math.abs(roundedB - b) < 1e-6) {
+                // Coefficients are integers, add to solution list
+                List<Integer> singleSolution = new ArrayList<>();
+                singleSolution.add(roundedA);
+                singleSolution.add(roundedB);
+                solutions.add(singleSolution);
+            } else {
+                return null; // Coefficients are not integers
+            }
+        }
+
+        return solutions;
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    private static int inverse_modulo_of_a_number(int a, int m) {
+        for (int i = 1; i < m; i++) {
+            if ( ( (a * i) - 1 ) % m == 0) {
+                return i;
+            }
+        }
+        return -1; // Inverse doesn't exist
+    }
+
+    private static String Decryption(int a, int b, String ciphertext, int m) {
+        StringBuilder plaintext = new StringBuilder();
+        int a_inverse_modulo = inverse_modulo_of_a_number(a, m);
+        if ( a_inverse_modulo == -1) {
+            throw new IllegalArgumentException("Inverse of 'a' doesn't exist.");
+        }
+        for (char single_character : ciphertext.toCharArray()) {
+            if (Character.isLetter(single_character)) {
+                char single_character_upper = Character.toUpperCase(single_character);
+                // Regularize the value of modulo in case of negative value by adding m to it
+                int decryptedChar = ((a_inverse_modulo * ((single_character_upper - 'A') - b)) % m + m ) % m;
+                char decrypted = (char) (decryptedChar + 'A');
+                plaintext.append(decrypted);
+            } else {
+                plaintext.append(single_character);
+            }
+        }
+        return plaintext.toString();
+    }
+
+    public static int numberOfDigramms(String plainText) {
+        ///20 most common digramms
+        String[] possibleDigramms = {
+                "TH", "HE", "IN", "ER", "AN", "RE", "ND", "ON", "EN", "AT",
+                "OU", "ED", "HA", "TO", "OR", "IT", "IS", "HI", "ES", "NG"
+        };
+
+        int foundDigramms = 0;
+        for (String digram : possibleDigramms) {
+            if (plainText.toUpperCase().contains(digram)) {
+                foundDigramms++;
+            }
+        }
+        return foundDigramms;
+    }
+
+
+    public static int numberOfTrigramms(String plainText) {
+        //20 most common trigramms
+        String[] possibleTrigramms = {
+                "THE", "AND", "ING", "HER", "HAT", "HIS", "THA", "ERE", "FOR",
+                "ENT", "ION", "TER", "WAS", "YOU", "ITH", "VER", "ALL", "WIT",
+                "THI", "TIO"
+        };
+
+        int foundTrigramms = 0;
+        for (String trigram : possibleTrigramms) {
+            if (plainText.toUpperCase().contains(trigram)) {
+                foundTrigramms++;
+            }
+        }
+        return foundTrigramms;
+    }
+
+    public static int numberOfQuadrigrams(String plainText) {
+        //20 most common quadrigrams
+        String[] possibleQuadrigrams = {
+                "THAT", "THER", "WITH", "TION", "HERE", "OULD", "IGHT", "HAVE",
+                "HICH", "WHIC", "THIS", "THIN", "THEY", "ATIO", "EVER", "FROM",
+                "OUGH", "WERE", "HING", "MENT"
+        };
+
+        int foundQuadrigrams = 0;
+        for (String quadrigram : possibleQuadrigrams) {
+            if (plainText.toUpperCase().contains(quadrigram)) {
+                foundQuadrigrams++;
+            }
+        }
+        return foundQuadrigrams;
+    }
 }
+
